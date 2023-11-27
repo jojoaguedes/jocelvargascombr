75c94ebd90b4fd40e2cea17060600771ded8e683
Jocel Vargas <jocelvargas@gmail.com>
2023-11-27 01:02:02 -0300

    adicionando coisas

---
+++ contatos.js
@@ -0,0 +1,40 @@
+import { Links } from "./links"
+
+const urlInstagram = "https://instagram.com/jocelvargasoficial";
+
+/*
+let redirect = document.querySelectorAll('.link');
+
+function navegarLinkEmNovaAba(url) {
+    window.open(url, '_blank');
+}
+
+function redirecionarLink() {
+    switch (redirect) {
+        case 'instagramLink':
+            navegarLinkEmNovaAba(Links.urlInstagram)
+            break;
+        case 'whatsappLink':
+            navegarLinkEmNovaAba(Links.urlWhatsapp)
+            break;
+        case 'spotifyLink':
+            navegarLinkEmNovaAba(Links.urlSpotify)
+            break;
+        case 'youtubeLink':
+            navegarLinkEmNovaAba(Links.urlYoutube)
+            break;
+        case 'appleLink':
+            navegarLinkEmNovaAba(Links.urlAppleMusic)
+            break;
+        case 'deezerLink':
+            navegarLinkEmNovaAba(Links.urlDeezer)
+            break;
+        case 'cifraClubLink':
+            navegarLinkEmNovaAba(Links.urlCifraClub)
+        default: null;
+            break;
+    }
+}
+
+redirecionarLink() 
+*/
